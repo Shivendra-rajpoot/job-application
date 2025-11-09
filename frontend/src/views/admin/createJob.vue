8ba4589d110d3d18bc9a2130ce5a9f2d3b1e7499
shibu@800 <ssshivendra09@gmail.com>
2025-11-09 20:33:52 +0530

    Initial commit - change index.html job vue

--- frontend/src/views/admin/createJob.vue
+++ frontend/src/views/admin/createJob.vue
@@ -28,49 +28,70 @@
                         <label for="code" class="form-label">Code</label>
                         <input type="text" class="form-control" id="code" placeholder="2021/A-2">
                      </div>
-                     <div class="col-12 col-lg-6">
-                        <label for="PhoneNumber" class="form-label">Mode of Application</label>
-                        <input type="text" class="form-control" id="PhoneNumber" placeholder="Phone Number">
-                     </div>
-                     <div class="col-12 col-lg-6">
-                        <label for="InputEmail" class="form-label">Category</label>
-                        <input type="text" class="form-control" id="InputEmail" placeholder="Enter Email Address">
-                     </div>
-                     <div class="col-12 col-lg-6">
+                    
+                                          <div class="col-12 col-lg-6">
                         <label for="advt_file" class="form-label">Advt file url</label>
                          <input type="text" class="form-control" id="advt_file" placeholder="Advt file url">
                      </div>
                      <div class="col-12 col-lg-6">
-                        <label for="InputCountry" class="form-label">From date</label>
-                        <select class="form-select" id="InputCountry" aria-label="Default select example">
-                           <option value="1">One</option>
-                           <option value="2">Two</option>
-                           <option value="3">Three</option>
+                        <label for="InputEmail" class="form-label">Category</label>
+                         <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="General" id="General" checked>
+									<label class="form-check-label" for="General">General</label>
+								</div>
+                                <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="OBC" id="OBC" checked>
+									<label class="form-check-label" for="OBC">OBC</label>
+								</div>
+                                <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="SC" id="SC" checked>
+									<label class="form-check-label" for="SC">SC</label>
+								</div>
+                                <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="ST" id="ST" checked>
+									<label class="form-check-label" for="ST">ST</label>
+								</div>
+                                <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="EWS" id="EWS" checked>
+									<label class="form-check-label" for="EWS">EWS</label>
+								</div>
+                     </div>
+                         <div class="col-12 col-lg-6">
+                        <label for="Mode of Application" class="form-label">Mode of Application</label>
+                        <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="Short Term Contract" id="Short Term Contract" checked>
+									<label class="form-check-label" for="Short Term Contract">Short Term Contract</label>
+								</div>
+                        <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="Direct Recruitment" id="Direct Recruitment">
+									<label class="form-check-label" for="Direct Recruitment">Direct Recruitment</label>
+								</div>
+                                <div class="form-check">
+									<input class="form-check-input" type="checkbox" value="Deputation" id="Deputation">
+									<label class="form-check-label" for="Deputation">Deputation</label>
+								</div>
+                                
+                                
+                     </div>
+                     <div class="col-12 col-lg-6">
+                        <label for="from_date" class="form-label">From date</label>
+                       <input type="date" class="form-control" name="from_date">
+                     </div>
+                     <div class="col-12 col-lg-6">
+                        <label for="to_date" class="form-label">To date</label>
+                        <input type="date" class="form-control" name="to_date">
+                     </div>
+                     <div class="col-12 col-lg-6">
+                        <label for="fee_apply" class="form-label">Fee Apply</label>
+                        <select class="form-select" id="fee_apply" name="fee_apply" aria-label="Default select example">
+                           <option value="Yes">Yes</option>
+                           <option value="No">No</option>
+                          
                         </select>
                      </div>
                      <div class="col-12 col-lg-6">
-                        <label for="InputLanguage" class="form-label">To date</label>
-                        <select class="form-select" id="InputLanguage" aria-label="Default select example">
-                           <option value="1">One</option>
-                           <option value="2">Two</option>
-                           <option value="3">Three</option>
-                        </select>
-                     </div>
-                     <div class="col-12 col-lg-6">
-                        <label for="InputLanguage" class="form-label">Fee Apply</label>
-                        <select class="form-select" id="InputLanguage" aria-label="Default select example">
-                           <option value="1">One</option>
-                           <option value="2">Two</option>
-                           <option value="3">Three</option>
-                        </select>
-                     </div>
-                     <div class="col-12 col-lg-6">
-                        <label for="InputLanguage" class="form-label">Fee</label>
-                        <select class="form-select" id="InputLanguage" aria-label="Default select example">
-                           <option value="1">One</option>
-                           <option value="2">Two</option>
-                           <option value="3">Three</option>
-                        </select>
+                        <label for="fee" class="form-label">Fee</label>
+                        <input type="text" name="fee" class="form-control" id="fee" placeholder="200">
                      </div>
                      
                      <div class="col-12 col-lg-6">
